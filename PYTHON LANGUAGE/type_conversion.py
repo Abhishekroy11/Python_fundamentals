2142c00a58c6d2e3a2f1a6f0b2438fd26ffc346f
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-01 09:34:31 -0800

    This is a part of Type Conversion which is generally convert the int to float,bool,string and etc.And here is also included the Type Casting process .

---
+++ PYTHON LANGUAGE/type_conversion.py
@@ -0,0 +1,19 @@
+'''This is Floating types of value.'''
+a = 10
+b = 5
+print(a/5)
+ans = 5+10.0
+print(type(ans))
+'''This is Type casting.
+As well as type Conversion.'''
+ans1 = int(5+10.0)#casting
+ans2= 5+10.2#Conversion
+print(ans1,type(ans1))
+print(ans2,type(ans2))
+'''In bollean case false is 0 and true is non-zero value.'''
+val=bool(10)
+print(val,type(val))
+'''In string value we convert in any types like int.'''
+val11="123"
+val11=int(val11)
+print(type(val11))
