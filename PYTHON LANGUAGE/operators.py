df926029cb5f0cb309d5644242e5e974cf8f2f48
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2025-12-31 20:31:09 -0800

    This is parts of OPERATORS in python language.This is also include OPERATOR PRECEDENCE.

---
+++ PYTHON LANGUAGE/operators.py
@@ -0,0 +1,41 @@
+''' This is Arithmatic operator
+for the operator learning purpose '''
+a = 10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+'''This is relational or comparison operator.'''
+a = 10
+b = 5
+print(a<b)
+a=10
+b=10
+print(a!=b)
+a=10
+b=5
+print(a!=b)
+a=10
+b=5
+print(a>=b)
+'''This is Assignment operator.'''
+a = 5
+a += 1
+print(a)
+b = 5
+b *= 3
+print(b)
+'''This is logical operator
+first is NOT oparator.'''
+var = False
+print (not var)
+print(not(8>5))
+'''AND operator.'''
+print((5>3) and (3>2))
+print((5>3) and (1>2))
+'''OR operator.'''
+print((5>2) or (3<5))
+print((2>5) or (5<3))
+'''OPERATOR PRECEDENCE.'''
+print(3*5+2)
