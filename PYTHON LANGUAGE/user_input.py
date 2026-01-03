c69959381df6ceeba8328efec410525e82af751c
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-02 19:01:46 -0800

    This is a basic type conversion using user input methods.

---
+++ PYTHON LANGUAGE/user_input.py
@@ -0,0 +1,16 @@
+'''This is a simple addition program,
+where it will add two string values because i do not mention its type(concatenation).'''
+a=input("Enter the value of a:")
+b=input("Enter the value of b:")
+sum=(a)+(b)
+print(sum)
+'''using type casting we can convert string into integer value and then add them.'''
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+sum=a+b
+print(sum)
+'''This is conversion float.'''
+a=float(input("Enter the value of a:"))
+b=float(input("Enter the value of b:"))
+sum=a+b
+print(sum)  
