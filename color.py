db1fa1296ba55b428fa24548edecd3f0b6e4317b
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-08 01:38:25 -0800

    This is the code of traffic control signals.

--- color.py
+++ color.py
@@ -2,8 +2,8 @@ color = input ("Enter a color: ")
 if color == "red":
     print("Stop")
 elif color == "Green":
-    print("Go")
+    print("go")
 elif color == "Yellow":
     print("Look")
 else:
-    print("Invalid color")
+    print("Invalid color!")
