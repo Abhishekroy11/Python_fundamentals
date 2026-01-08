165c00cac9d5735a00851c2f38ec917b2eca6ec0
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-07 23:42:01 -0800

    Save the python files.

--- ques_7.py
+++
@@ -1,9 +0,0 @@
-celsius_str = input("Enter temperature in Celsius: ")
-
-# convert string to float
-celsius = float(celsius_str)
-
-# conversion formula
-fahrenheit = (celsius * (9/5)) + 32
-
-print("Temperature in Fahrenheit =", fahrenheit)
