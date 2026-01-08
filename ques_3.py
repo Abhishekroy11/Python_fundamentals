65990bd42b700f563b1bc40b1fae8fe1f079b700
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-07 23:13:39 -0800

    This is the basic code assignemnt

---
+++ ques_3.py
@@ -0,0 +1,17 @@
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = float(input("Enter one float number: "))
+
+# convert all to float
+a = float(a)
+b = float(b)
+
+average = (a + b + c) / 3
+
+print("Average =", average)
+# convert all to int
+a = int(a)
+b = int(b)
+c = int(c)  
+product = a * b * c
+print("Product =", product)
