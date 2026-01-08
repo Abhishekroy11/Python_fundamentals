bfae63591161639c23e381de97385c1bb06179e3
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-01-08 01:13:32 -0800

    Traffic signal code.

---
+++ color.py
@@ -0,0 +1,9 @@
+color = input ("Enter a color: ")
+if color == "red":
+    print("Stop")
+elif color == "Green":
+    print("Go")
+elif color == "Yellow":
+    print("Look")
+else:
+    print("Invalid color")
